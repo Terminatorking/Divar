4102762b45ba949b927017965a9c4427735680b0
Soheil <soheilghazimoradi@gmail.com>
2025-03-23 23:48:40 +0330

    create category mappers and repositories and services (forgotten files)

--- core/network/src/main/java/ghazimoradi/soheil/divar/network/di/ProvideApiServices.kt
+++ core/network/src/main/java/ghazimoradi/soheil/divar/network/di/ProvideApiServices.kt
@@ -4,7 +4,13 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import ghazimoradi.soheil.divar.network.api.CategoryApiService
+import ghazimoradi.soheil.divar.network.api.ads.AdsApiService
+import ghazimoradi.soheil.divar.network.api.ads.AdsSummaryApiService
+import ghazimoradi.soheil.divar.network.api.category.CategoryApiService
+import ghazimoradi.soheil.divar.network.api.location.LocationApiService
+import ghazimoradi.soheil.divar.network.api.category.CategoryOfAdsApiService
+import ghazimoradi.soheil.divar.network.api.parameter.ParameterApiService
+import ghazimoradi.soheil.divar.network.api.user.UserApiService
 import retrofit2.Retrofit
 import javax.inject.Singleton
 
@@ -16,4 +22,41 @@ object ProvideApiServices {
     fun provideCategoryApiService(retrofit: Retrofit): CategoryApiService {
         return retrofit.create(CategoryApiService::class.java)
     }
+
+    @Singleton
+    @Provides
+    fun provideAdsSummaryApiService(retrofit: Retrofit): AdsSummaryApiService {
+        return retrofit.create(AdsSummaryApiService::class.java)
+    }
+
+    @Singleton
+    @Provides
+    fun provideLocationApiService(retrofit: Retrofit): LocationApiService {
+        return retrofit.create(LocationApiService::class.java)
+    }
+
+    @Singleton
+    @Provides
+    fun provideCategoryOfAdsApiService(retrofit: Retrofit): CategoryOfAdsApiService {
+        return retrofit.create(CategoryOfAdsApiService::class.java)
+    }
+
+    @Singleton
+    @Provides
+    fun provideParametersApiService(retrofit: Retrofit): ParameterApiService {
+        return retrofit.create(ParameterApiService::class.java)
+    }
+
+    @Singleton
+    @Provides
+    fun provideAdsApiService(retrofit: Retrofit): AdsApiService {
+        return retrofit.create(AdsApiService::class.java)
+    }
+
+    @Singleton
+    @Provides
+    fun provideUserApiService(retrofit: Retrofit): UserApiService {
+        return retrofit.create(UserApiService::class.java)
+    }
+
 }
---
+++ domain/src/main/java/ghazimoradi/soheil/divar/domain/model/ads/CreateAdsParam.kt
@@ -0,0 +1,19 @@
+package ghazimoradi.soheil.divar.domain.model.ads
+
+import ghazimoradi.soheil.divar.domain.model.category.Category
+import ghazimoradi.soheil.divar.domain.model.location.NeighbourHood
+import ghazimoradi.soheil.divar.domain.model.parameter.Parameter
+import kotlinx.collections.immutable.ImmutableList
+import kotlinx.collections.immutable.toImmutableList
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class CreateAdsParam(
+    val category: Category? = null,
+    val neighborhood: NeighbourHood? = null,
+    val images: ImmutableList<String> = listOf("", "", "", "", "", "").toImmutableList(),
+    val title: String = "",
+    val description: String = "",
+    val price: String = "",
+    val parameters: ImmutableList<Parameter> = listOf<Parameter>().toImmutableList()
+)
