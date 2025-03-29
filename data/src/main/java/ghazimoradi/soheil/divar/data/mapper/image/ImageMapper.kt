c5eb0ff7bbbe8f088447817bae3ada1057760524
Soheil <soheilghazimoradi@gmail.com>
2025-03-29 22:08:40 +0330

    update ImageMapper.kt

--- data/src/main/java/ghazimoradi/soheil/divar/data/mapper/image/ImageMapper.kt
+++ data/src/main/java/ghazimoradi/soheil/divar/data/mapper/image/ImageMapper.kt
@@ -3,13 +3,9 @@ package ghazimoradi.soheil.divar.data.mapper.image
 import ghazimoradi.soheil.divar.data.BuildConfig
 import ghazimoradi.soheil.divar.domain.model.image.Image
 import ghazimoradi.soheil.divar.network.dto.image.ImageResponse
+import ghazimoradi.soheil.divar.ui.extension.eLog
 
 fun ImageResponse.toDomain(): Image {
-    return Image(path = BuildConfig.BaseUrl + imageUrl(this.path))
-}
-
-private fun imageUrl(path: String): String {
-    val splitText: List<String> = path.split("\\\\")
-    val lastIndex = splitText.lastIndex
-    return splitText[lastIndex]
+    (BuildConfig.BaseUrl + this.path).eLog(tag = "image")
+    return Image(path = BuildConfig.BaseUrl + this.path)
 }
